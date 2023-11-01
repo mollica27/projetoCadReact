4ce1cddfd86b9a3d2330a8f4b189937ccf7a0925
Natan Mollica <natan.mollica@gmail.com>
2023-11-01 11:36:54 -0300

    teste enviar

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,23 @@ export default class Main extends Component {
     };
     handleChange = (e) => {
         this.setState({
+            novaTarefa: e.target.value,
+        });
+    }
 
-        })
+    render() {
+        const { novaTarefa } = this.state
+
+        return (
+            <div className="main">
+                <h1>{novaTarefa}</h1>
+
+                <form action="#">
+                    <input onChange={this.handleChange} type="text" />
+                    <button type="submit">Enviar</button>
+
+                </form>
+            </div>
+        )
     }
 }
